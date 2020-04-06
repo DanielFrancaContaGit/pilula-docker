f89b555ec4330ee0ce18ee8b1f580044255f6540
Daniel <daniel.pe.15y15@gmail.com>
2020-04-06 19:45:49 -0300

    gif change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import gif from './boy-do-computador.gif';
+import gif from './leonardo-de-caprio.gif';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={gif}/>
+        <img src={gif} width="350px"/>
         <p>
         Congratulations you got it!
         </p>
